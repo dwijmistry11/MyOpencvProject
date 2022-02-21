c47e35e59876b5edcd18d5b2427268fd9c3ae5dc
Dwij Mistry <dwijmistry11@gmail.com>
2022-02-21 08:38:22 +0530

    updated

--- ResizeAndCroping.py
+++ ResizeAndCroping.py
@@ -3,10 +3,14 @@ import numpy as np
 
 img = cv2.imread("Resources/baboon.png")
 
-#findout size of image
+#print size of an image with color channel number
 print(img.shape)
 
+#Resize an image
+imgResize = cv2.resize(img,(300,200))
+print(imgResize.shape)
 
-cv2.imshow("Image",img)
+cv2.imshow("Image", img)
+cv2.imshow("Image Resize", imgResize)
 
 cv2.waitKey(0)
